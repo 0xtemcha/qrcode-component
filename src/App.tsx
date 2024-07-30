93a61760776fb86a528ef57b6359b400fcd90d87
temcha <itemchur@spin.ai>
2024-07-31 00:26:32 +0300

    added assets and basic html with text

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <h1>App</h1>;
+  return (
+    <h1>
+      Improve your front-end skills by building projects Scan the QR code to
+      visit Frontend Mentor and take your coding skills to the next level
+    </h1>
+  );
 }
 
 export default App;
